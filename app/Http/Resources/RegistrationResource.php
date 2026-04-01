4862d776cb3683cb115e0c4fc9126d147b40a46d
Jaypee P Zulieta <jaypee.zulieta@sccpag.edu.ph>
2026-04-01 19:36:48 +0800

    Rearrange the order of the fields in the RegistrationResource

--- app/Http/Resources/RegistrationResource.php
+++ app/Http/Resources/RegistrationResource.php
@@ -16,6 +16,7 @@ class RegistrationResource extends JsonResource
     {
         return [
             "registration_id" => $this->id,
+            "registration_date" => $this->registration_date,
             "cadet_information" => new StudentCadetResource($this->studentCadet),
             "school" => $this->school,
             "academic_course" => $this->academic_course,
@@ -23,7 +24,6 @@ class RegistrationResource extends JsonResource
             "religion" => $this->religion,
             "height_m" => $this->height_m,
             "weight_kg" => $this->weight_kg,
-            "registration_date" => $this->registration_date,
             "contact_number" => $this->contact_number,
             "semester" => $this->semester,
             "school_year" => $this->school_year,
