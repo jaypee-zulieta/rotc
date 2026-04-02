cc28cb9c56f5a8d3a216582590961e4eae3501e8
Jaypee P Zulieta <jaypee.zulieta@sccpag.edu.ph>
2026-04-02 15:11:31 +0800

    Add impl for RegistrationService.store()

--- app/Services/RegistrationService.php
+++ app/Services/RegistrationService.php
@@ -6,6 +6,10 @@ use App\Http\Requests\StoreRegistrationRequest;
 use App\Http\Resources\RegistrationResource;
 use Illuminate\Http\Request;
 use App\Models\Registration;
+use Illuminate\Support\Facades\DB;
+use App\Models\Address;
+use App\Models\BirthDetails;
+use App\Models\StudentCadet;
 
 class RegistrationService
 {
@@ -35,6 +39,46 @@ class RegistrationService
     public function store(StoreRegistrationRequest $request)
     {
         $validated = $request->validated();
-        return $validated;
+
+        return DB::transaction(function () use ($validated) {
+
+            // If it finds an address with the same details it will use that, if it doesn't it will make a new record
+            $address = Address::firstOrCreate($validated['cadet_information']['birth_details']['birth_place']);
+
+            $birthDetails = BirthDetails::firstOrCreate([
+                "date_of_birth" => $validated['cadet_information']['birth_details']['date_of_birth'],
+                "address_id" => $address->id
+            ]);
+
+            $studentCadet = StudentCadet::updateOrCreate([
+                "student_number" => $validated['cadet_information']['student_number']
+            ], [
+                "first_name" => $validated['cadet_information']['first_name'],
+                "middle_name" => data_get($validated['cadet_information'], 'middle_name'),
+                "last_name" => $validated['cadet_information']['last_name'],
+                "suffix" => data_get($validated['cadet_information'], 'suffix'),
+                "complexion" => data_get($validated['cadet_information'], 'complexion'),
+                "blood_type" => data_get($validated['cadet_information'], 'blood_type'),
+                "sex" => $validated['cadet_information']['sex'],
+                "birth_details_id" => $birthDetails->id,
+            ]);
+
+            $registration = Registration::create([
+                "student_cadet_id" => $studentCadet->id,
+                "registration_date" => $validated['registration_date'],
+                "school" => $validated['school'],
+                "academic_course" => $validated['academic_course'],
+                "military_course" => $validated['military_course'],
+                "religion" => $validated['religion'],
+                "height_m" => $validated['height_m'],
+                "weight_kg" => $validated['weight_kg'],
+                "contact_number" => $validated['contact_number'],
+                "semester" => $validated['semester'],
+                "school_year" => $validated['school_year'],
+                "is_willing_to_take_advance_course" => data_get($validated, 'is_willing_to_take_advance_course', false),
+            ]);
+
+            return new RegistrationResource($registration);
+        });
     }
 }
